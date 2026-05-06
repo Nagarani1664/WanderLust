e8d644df3898b0e94a27dbd7601207a79d8377dd
Nagarani1664 <Nagarani.Immadishetty@gmail.com>
2026-05-06 23:49:37 +0530

    Fixed MongoStore issues

--- schema.js
+++ schema.js
@@ -1,13 +1,14 @@
 const Joi=require("joi");
 
-const listingSchema=Joi.object({
+const listingSchema = Joi.object({
     listing : Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        location:Joi.string().required(),
-        price:Joi.number().required().min(500),
-        country:Joi.string().required(),
-        image:Joi.string().allow("",null)
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        category: Joi.string().required(),
+        location: Joi.string().required(),
+        price: Joi.number().required().min(500),
+        country: Joi.string().required(),
+        image: Joi.string().allow("", null)
     }).required()
 });
 
